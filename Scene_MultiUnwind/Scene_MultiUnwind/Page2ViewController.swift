00d8e3ac796691c67d6b96a47745514e83a24adf
한경동 <hkd0694@gmail.com>
2021-12-29 17:00:19 +0900

    Scene_MultiUnwind Sample Code

---
+++ Scene_MultiUnwind/Scene_MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Page2ViewController.swift
+//  Scene_MultiUnwind
+//
+//  Created by KyungDong Han on 2021/12/29.
+//
+
+import UIKit
+class Page2ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func gotoPage2(_ sender: UIStoryboardSegue) {
+        
+    }
+    
+}
---
+++ Scene_MultiUnwind/Scene_MultiUnwind/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Scene_MultiUnwind
+//
+//  Created by KyungDong Han on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
