fddf6ed50b8f3b4562f7d74c21b2af398a7fa4d3
한경동 <hkd0694@gmail.com>
2021-12-30 13:39:10 +0900

    Msg-Notification initial commit

---
+++ Msg-Notification/Msg-Notification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Msg-Notification
+//
+//  Created by KyungDong Han on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
