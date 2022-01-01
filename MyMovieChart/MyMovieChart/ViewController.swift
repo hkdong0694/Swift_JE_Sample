8c5986ff3227b20ce13a9103e8c8254764da0799
한경동 <hkd0694@gmail.com>
2022-01-01 20:06:39 +0900

    MyMovieChart Initial commit

---
+++ MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by KyungDong Han on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
