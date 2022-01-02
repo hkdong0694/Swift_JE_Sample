eefbc5fd20d1b83a250df34562eb397a38f8725d
한경동 <hkd0694@gmail.com>
2022-01-02 15:38:46 +0900

    TableView 하드코딩 된 데이터 연결 완료

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -8,4 +8,49 @@
 import UIKit
 class ListViewController : UITableViewController {
     
+    var dataset = [
+        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-15", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2010-05-07", 9.91)
+    ]
+    
+    // 테이블 뷰를 구성할 리스트 데이터
+    lazy var list : [MovieVO] = {
+        var datalist = [MovieVO]()
+        for( title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    // 리스트 갯수 반환 -> TableViewController 에서 가장 먼저 실행되는 Method
+    // Android -> getCount 동일
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    // Android -> onBind 와 같은 역할 Method
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        return cell
+    }
+    
+    // 행을 선택했을 때 호출되는 Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
+    }
+    
+    
 }
--- MyMovieChart/MyMovieChart/ViewController.swift
+++ MyMovieChart/MyMovieChart/ViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        // 첫 번째 행
+//        var mvo = MovieVO()
+//        mvo.title = "다크나이트"
+//        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
+//        mvo.opendate = "2008-09-04"
+//        mvo.rating = 8.95
+//        // 배열에 추가
+//        self.list.append(mvo)
+//
+//        // 두번째 행
+//        mvo = MovieVO()
+//        mvo.title = "호우시절"
+//        mvo.description = "때를 알고 내리는 좋은 비"
+//        mvo.opendate = "2009-10-04"
+//        // 배열에 추가
+//        mvo.rating = 7.31
+//        self.list.append(mvo)
+//
+//        // 세번째 행
+//        mvo = MovieVO()
+//        mvo.title = "말할 수 없는 비밀"
+//        mvo.description = "여기서 너까지 다섯 걸음"
+//        mvo.opendate = "2010-05-7"
+//        mvo.rating = 9.00
+//        // 배열에 추가
+//        self.list.append(mvo)
+
+        
     }
 
 
