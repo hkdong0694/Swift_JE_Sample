05c9abdd26875b0abb7a82f43bde9e0d69b80955
한경동 <hkd0694@gmail.com>
2022-01-07 17:24:13 +0900

    WebView 관련 프로토콜 정의 완료

--- MyMovieChart_WebView/MyMovieChart_WebView/Controller/DetailViewController.swift
+++ MyMovieChart_WebView/MyMovieChart_WebView/Controller/DetailViewController.swift
@@ -12,10 +12,17 @@ class DetailViewController: UIViewController {
     
     @IBOutlet var wv: WKWebView!
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     var mvo: MovieVO! // 목록 화면에서 전달하는 영화 정보를 받을 변수
     
     
     override func viewDidLoad() {
+        
+        // WKNavigationDelegate의 델리게이트 객체를 지정
+        self.wv.navigationDelegate = self
+        // WKUIDelegate의 델리게이트 객체를 지정
+        // self.wv.uiDelegate = self
         
         let navibar = self.navigationItem
         navibar.title = self.mvo?.title
@@ -49,5 +56,51 @@ class DetailViewController: UIViewController {
             alert.addAction(cancelAction)
             self.present(alert, animated: false, completion: nil)
         }
+    }
+}
+
+// MARK:- WKNavigationDelegate 프로토콜 구현
+extension DetailViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        // 인디케이터 뷰의 애니메이션을 실행
+        self.spinner.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // 인디케이터 뷰의 애니메이션을 중단
+        self.spinner.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        // 인디케이터 뷰의 애니메이션을 중단
+        self.spinner.stopAnimating()
+        self.alert("상세 페이지를 읽어오지 못했습니다.") {
+            _ = self.navigationController?.popViewController(animated: false)
+        }
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        self.spinner.stopAnimating()
+        // 버튼 클릭시, 이전 화면으로 되돌려 보낸다.
+        self.alert("상세 페이지를 읽어오지 못했습니다.")  {
+            _ = self.navigationController?.popViewController(animated: false)
+        }
+    }
+    
+}
+
+// MARK:- 심플한 경고창 함수 정의용 인스텐션
+extension UIViewController {
+    func alert(_ message: String, onClick: (() -> Void)? = nil) {
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel){
+            (_) in onClick?()
+        })
+        
+        DispatchQueue.main.async {
+            self.present(controller, animated: false)
+        }
+        
     }
 }
