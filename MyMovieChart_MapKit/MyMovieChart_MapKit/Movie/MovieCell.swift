b0baac9e28034087574f15febbf7effece5af6d8
한경동 <hkd0694@gmail.com>
2022-01-11 19:57:10 +0900

    Commit2

---
+++ MyMovieChart_MapKit/MyMovieChart_MapKit/Movie/MovieCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieCell.swift
+//  MyMovieChart_MapKit
+//
+//  Created by KyungDong Han on 2022/01/11.
+//
+import UIKit
+class MovieCell : UITableViewCell {
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+    @IBOutlet var title: UILabel!
+    
+    @IBOutlet var desc: UILabel!
+    
+    @IBOutlet var opendate: UILabel!
+    
+    @IBOutlet var rating: UILabel!
+    
+    
+}
