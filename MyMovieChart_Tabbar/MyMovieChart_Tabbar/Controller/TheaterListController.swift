b6a0a2120a5efd3eef0970ce4e729fb4c8425649
한경동 <hkd0694@gmail.com>
2022-01-11 18:43:49 +0900

    탭 바 컨트롤러 샘플 코드 완료

--- MyMovieChart_Tabbar/MyMovieChart_Tabbar/Controller/TheaterListController.swift
+++ MyMovieChart_Tabbar/MyMovieChart_Tabbar/Controller/TheaterListController.swift
@@ -21,7 +21,44 @@ class TheaterListController : UITableViewController {
     
     // OpenAPI 호출
     fileprivate func callOpenAPI() {
+        // URL 을 구성하기 위한 상수값을 선언한다.
+        let requestURL = "http://swiftapi.rubypaper.co.kr:2029/theater/list" // API 요청 주소
         
+        let sList = 100
+        let type = "json"
+        
+        // 인자값들을 모아 URL 객체로 정의한다
+        let urlObj = URL(string: "\(requestURL)?s_page=\(self.startPoint)&s_list=\(sList)&type=\(type)")
+        
+        do {
+            // NSString 객체를 이용하여 API를 호출하고 그 결과값을 인코딩된 문자열로 받아온다.
+            let stringdata = try NSString(contentsOf: urlObj!, encoding: 0x80_000_422)
+            // 문자열로 받은 데이터를 UTF-8 로 인코딩처리한 Data로 변환한다.
+            let encdata = stringdata.data(using: String.Encoding.utf8.rawValue)
+            
+            do {
+                // Data 객체를 파싱하여 NSArray 객체로 변환한다.
+                let apiArray = try JSONSerialization.jsonObject(with: encdata!, options: []) as? NSArray
+                
+                for obj in apiArray! {
+                    self.list.append(obj as! NSDictionary)
+                }
+                
+            } catch {
+                alert("데이터 분석이 실패하였습니다.", onClick: nil)
+            }
+            
+            self.startPoint += sList
+            
+        } catch {
+            alert("데이터를 불러오는데 실패하였습니다.", onClick: nil)
+        }
+        
+    }
+    @IBAction func moreBtn(_ sender: Any) {
+        
+        self.callOpenAPI()
+        self.tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,13 +71,28 @@ class TheaterListController : UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "tCell") as! TheaterCell
         
+        cell.name?.text = row["상영관명"] as? String
+        cell.tel?.text = row["연락처"] as? String
+        cell.addr?.text = row["소재지도로명주소"] as? String
         return cell
-        
     }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-    }
+}
+
+
+extension TheaterListController {
     
+    func alert(_ message : String, onClick: (() -> Void)? = nil)  {
+        
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel){
+            (_) in
+            onClick?()
+        })
+        
+        DispatchQueue.main.async {
+            self.present(controller, animated: false)
+        }
+    }
 }
